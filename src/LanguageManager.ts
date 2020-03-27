d8d22e279dbf036523be0874ae42cddf40a881ee
Lucas Segurado <segurado1996@gmail.com>
2020-03-27 14:21:06 -0300

    Added onLanguageChange callback to language manager

--- src/LanguageManager.ts
+++ src/LanguageManager.ts
@@ -1,13 +1,19 @@
 import I18nUtils from "./I18nUtils";
 
+export interface OnLanguageChangeFunction {
+    (newLanguage: string): void;
+}
+
 export default class LanguageManager {
-    constructor() {
+    constructor(onLanguageChange: OnLanguageChangeFunction) {
         const localStorageResult = localStorage.getItem('lang');
         const lang = localStorageResult === null ? 'en' : localStorageResult;
         I18nUtils.setLanguage(lang);
+        onLanguageChange(lang);
 
         new MutationObserver(() => {
             localStorage.setItem('lang', I18nUtils.getDocumentLanguage());
+            onLanguageChange(I18nUtils.getDocumentLanguage());
         }).observe(document.documentElement, {
             attributes: true, //configure it to listen to attribute changes
             attributeFilter: ['lang']
